88b0520fe8206399b505e7170ef2d1ba94e3be9b
Ekaterina Serga <ekaterina.a.serga@gmail.com>
2020-08-19 17:04:59 -0400

    save: work progress, shop page

--- client/components/productList.js
+++ client/components/productList.js
@@ -1,40 +1,66 @@
 import React from 'react'
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart'
 
 class ProductList extends React.Component {
   render() {
     return (
       <div className="list-ctn">
         <img src={this.props.product.imageUrl} />
-        <h1 style={{fontSize: '25px'}}>{this.props.product.name}</h1>
+        <h1>{this.props.product.name}</h1>
         {/* <p>{this.props.products.description}</p> */}
         <div id="priceCart">
           <p
             style={{
               fontSize: '25px',
               color: 'white',
-              width: '30%',
+              width: '50%',
               textAlign: 'center'
             }}
           >
             ${this.props.product.price}
           </p>
-          <input
+          {/* <input
             type="number"
             defaultValue="1"
             onChange={this.props.change}
             min="1"
             max="20"
             style={{width: '20%'}}
-          />
-          <button
-            style={{width: '50%'}}
-            onClick={this.props.addToCart}
-            id={this.props.product.id}
-            type="button"
-          >
-            Add To Cart
-          </button>
+          /> */}
+          <div className="quantity-control">
+            <button className="quantity-btn">
+              <svg viewBox="0 0 409.6 409.6">
+                <g>
+                  <g>
+                    <path d="M392.533,187.733H17.067C7.641,187.733,0,195.374,0,204.8s7.641,17.067,17.067,17.067h375.467 c9.426,0,17.067-7.641,17.067-17.067S401.959,187.733,392.533,187.733z" />
+                  </g>
+                </g>
+              </svg>
+            </button>
+            <input
+              type="number"
+              className="quantity-input"
+              defaultValue="1"
+              onChange={this.props.change}
+              min="1"
+              max="20"
+              name="quantity"
+            />
+            <button className="quantity-btn">
+              <svg viewBox="0 0 426.66667 426.66667">
+                <path d="m405.332031 192h-170.664062v-170.667969c0-11.773437-9.558594-21.332031-21.335938-21.332031-11.773437 0-21.332031 9.558594-21.332031 21.332031v170.667969h-170.667969c-11.773437 0-21.332031 9.558594-21.332031 21.332031 0 11.777344 9.558594 21.335938 21.332031 21.335938h170.667969v170.664062c0 11.777344 9.558594 21.335938 21.332031 21.335938 11.777344 0 21.335938-9.558594 21.335938-21.335938v-170.664062h170.664062c11.777344 0 21.335938-9.558594 21.335938-21.335938 0-11.773437-9.558594-21.332031-21.335938-21.332031zm0 0" />
+              </svg>
+            </button>
+          </div>
         </div>
+        <button
+          onClick={this.props.addToCart}
+          id={this.props.product.id}
+          type="button"
+          className="addToCart"
+        >
+          <ShoppingCartIcon />
+        </button>
       </div>
     )
   }
