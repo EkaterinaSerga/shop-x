71fad51bbe5b99ec1f2afd78c7d9b0fc9d0625ed
Ekaterina Serga <ekaterina.a.serga@gmail.com>
2021-02-01 22:33:19 -0500

    feature(FE): added add and subtract functionality to buttons in ProductList, refactored code for AllProducts

--- client/components/allProducts.js
+++ client/components/allProducts.js
@@ -1,54 +1,14 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {fetchProducts, deleteProduct} from '../store/allProducts'
-import ProductList from './productList'
+import ProductList from './ProductList'
 import {Link} from 'react-router-dom'
 
 class AllProducts extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      qty: 1
-    }
-    this.addToCart = this.addToCart.bind(this)
-    this.handleChange = this.handleChange.bind(this)
-  }
-
   componentDidMount() {
     this.props.fetchProducts()
   }
 
-  addToCart(event) {
-    const productToAdd = {
-      id: this.props.products[event.target.id - 1].id,
-      name: this.props.products[event.target.id - 1].name,
-      price: this.props.products[event.target.id - 1].price,
-      quantity: Number(this.state.qty),
-      imageUrl: this.props.products[event.target.id - 1].imageUrl
-    }
-
-    if (!localStorage.getItem('cart')) {
-      localStorage.setItem('cart', JSON.stringify([productToAdd]))
-    } else {
-      const newCart = JSON.parse(localStorage.getItem('cart'))
-      const product = newCart.find(item => item.id === Number(event.target.id))
-      if (product) {
-        product.quantity = Number(product.quantity) + Number(this.state.qty)
-      } else {
-        newCart.push(productToAdd)
-      }
-      localStorage.setItem('cart', JSON.stringify(newCart))
-    }
-    this.setState({
-      qty: 1
-    })
-  }
-
-  handleChange(event) {
-    this.setState({
-      qty: event.target.value
-    })
-  }
   render() {
     return (
       <div className="allProducts">
@@ -56,12 +16,7 @@ class AllProducts extends React.Component {
           <h2>There are no products registered in the database</h2>
         ) : (
           this.props.products.map(exp => (
-            <ProductList
-              key={exp.id}
-              product={exp}
-              change={this.handleChange}
-              addToCart={this.addToCart}
-            />
+            <ProductList key={exp.id} product={exp} />
           ))
         )}
       </div>
--- client/components/productList.js
+++ client/components/productList.js
@@ -3,6 +3,73 @@ import ShoppingCartIcon from '@material-ui/icons/ShoppingCart'
 import {Link} from 'react-router-dom'
 
 class ProductList extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      qty: 1
+    }
+    this.addToCart = this.addToCart.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.add = this.add.bind(this)
+    this.subtract = this.subtract.bind(this)
+    this.valueCheck = this.valueCheck.bind(this)
+  }
+
+  addToCart(event) {
+    const productToAdd = {
+      id: this.props.product.id,
+      name: this.props.product.name,
+      price: this.props.product.price,
+      quantity: Number(this.state.qty),
+      imageUrl: this.props.product.imageUrl
+    }
+
+    if (!localStorage.getItem('cart')) {
+      localStorage.setItem('cart', JSON.stringify([productToAdd]))
+    } else {
+      const newCart = JSON.parse(localStorage.getItem('cart'))
+      const product = newCart.find(item => item.id === Number(event.target.id))
+      if (product) {
+        product.quantity = Number(product.quantity) + Number(this.state.qty)
+      } else {
+        newCart.push(productToAdd)
+      }
+      localStorage.setItem('cart', JSON.stringify(newCart))
+    }
+    alert('Added to cart!')
+    this.setState({
+      qty: 1
+    })
+  }
+
+  valueCheck(value) {
+    if (value < 1 || value > 20) {
+      alert('This is an invalid input, please select a number between 1 and 20')
+      throw new Error('Invalid value')
+    }
+  }
+
+  handleChange(event) {
+    this.valueCheck(event.target.value)
+    this.setState({
+      qty: parseInt(event.target.value)
+    })
+  }
+
+  add() {
+    this.valueCheck(this.state.qty + 1)
+    this.setState({
+      qty: this.state.qty + 1
+    })
+  }
+
+  subtract() {
+    this.valueCheck(this.state.qty - 1)
+    this.setState({
+      qty: this.state.qty - 1
+    })
+  }
+
   render() {
     const {product} = this.props
     return (
@@ -11,7 +78,6 @@ class ProductList extends React.Component {
           <img src={product.imageUrl} className="list-item-image" />
           <h1>{product.name}</h1>
         </Link>
-        {/* <p>{product.description}</p> */}
         <div id="priceCart">
           <p
             style={{
@@ -23,16 +89,8 @@ class ProductList extends React.Component {
           >
             ${product.price}
           </p>
-          {/* <input
-            type="number"
-            defaultValue="1"
-            onChange={this.props.change}
-            min="1"
-            max="20"
-            style={{width: '20%'}}
-          /> */}
           <div className="quantity-control">
-            <button className="quantity-btn">
+            <button className="quantity-btn minus" onClick={this.subtract}>
               <svg viewBox="0 0 409.6 409.6">
                 <g>
                   <g>
@@ -45,12 +103,13 @@ class ProductList extends React.Component {
               type="number"
               className="quantity-input"
               defaultValue="1"
-              onChange={this.props.change}
+              onChange={this.handleChange}
               min="1"
               max="20"
               name="quantity"
+              value={this.state.qty}
             />
-            <button className="quantity-btn">
+            <button className="quantity-btn plus" onClick={this.add}>
               <svg viewBox="0 0 426.66667 426.66667">
                 <path d="m405.332031 192h-170.664062v-170.667969c0-11.773437-9.558594-21.332031-21.335938-21.332031-11.773437 0-21.332031 9.558594-21.332031 21.332031v170.667969h-170.667969c-11.773437 0-21.332031 9.558594-21.332031 21.332031 0 11.777344 9.558594 21.335938 21.332031 21.335938h170.667969v170.664062c0 11.777344 9.558594 21.335938 21.332031 21.335938 11.777344 0 21.335938-9.558594 21.335938-21.335938v-170.664062h170.664062c11.777344 0 21.335938-9.558594 21.335938-21.335938 0-11.773437-9.558594-21.332031-21.335938-21.332031zm0 0" />
               </svg>
@@ -58,7 +117,7 @@ class ProductList extends React.Component {
           </div>
         </div>
         <button
-          onClick={this.props.addToCart}
+          onClick={this.addToCart}
           id={product.id}
           type="button"
           className="addToCart"
